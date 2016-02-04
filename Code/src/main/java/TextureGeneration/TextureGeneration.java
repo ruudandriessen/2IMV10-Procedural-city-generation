6263a4deeaf70bd41cc15174c5f6adaf757db820
Ruud Andriessen <r.andriessen@ilysian.com>
2016-02-04 16:23:47 +0100

    Fixed compile errors after lwjgl version switch

--- Code/src/main/java/TextureGeneration/TextureGeneration.java
+++ Code/src/main/java/TextureGeneration/TextureGeneration.java
@@ -1,189 +1,33 @@
 package TextureGeneration;
 
-import java.nio.FloatBuffer;
-import org.lwjgl.*;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
- 
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryUtil.*;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
  
 public class TextureGeneration {
-
-    // We need to strongly reference callback instances.
-    private GLFWErrorCallback errorCallback;
-    private GLFWKeyCallback   keyCallback;
- 
-    // The window handle
-    private long window;
-    
-    // Vertex count and buffer ids
-    private int vertexCount, vaoId, vboId;
- 
-    public void run() {
-        System.out.println("Procedural city generation");
- 
+    public void start() {
         try {
-            init();
-            loop();
- 
-            // Release window and window callbacks
-            glfwDestroyWindow(window);
-            keyCallback.release();
-        } finally {
-            cleanup();
+            Display.setDisplayMode(new DisplayMode(800,600));
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            System.exit(0);
         }
-    }
- 
-    private void init() {
-        // Setup an error callback. The default implementation
-        // will print the error message in System.err.
-        glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
- 
-        // Initialize GLFW. Most GLFW functions will not work before doing this.
-        if ( glfwInit() != GLFW_TRUE )
-            throw new IllegalStateException("Unable to initialize GLFW");
- 
-        // Configure our window
-        glfwDefaultWindowHints(); // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
- 
-        int WIDTH = 300;
-        int HEIGHT = 300;
- 
-        // Create the window
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
-        if ( window == NULL )
-            throw new RuntimeException("Failed to create the GLFW window");
- 
-        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-        glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
-            @Override
-            public void invoke(long window, int key, int scancode, int action, int mods) {
-                if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-                    glfwSetWindowShouldClose(window, GLFW_TRUE); // We will detect this in our rendering loop
-            }
-        });
- 
-        // Get the resolution of the primary monitor
-        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-        // Center our window
-        glfwSetWindowPos(
-            window,
-            (vidmode.width() - WIDTH) / 2,
-            (vidmode.height() - HEIGHT) / 2
-        );
- 
-        // Make the OpenGL context current
-        glfwMakeContextCurrent(window);
-        // Enable v-sync
-        glfwSwapInterval(1);
- 
-        // Make the window visible
-        glfwShowWindow(window);
-        
-//        createVertices();
-    }
-    
-    private void cleanup() {
-        // Disable the VBO index from the VAO attributes list
-        GL20.glDisableVertexAttribArray(0);
-
-        // Delete the VBO
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-        GL15.glDeleteBuffers(vboId);
-
-        // Delete the VAO
-        GL30.glBindVertexArray(0);
-        GL30.glDeleteVertexArrays(vaoId);
-        
-        // Terminate GLFW and release the GLFWErrorCallback
-        glfwTerminate();
-        errorCallback.release();
-    }
-    
-    private void createVertices() {
-        float[] vertices = {
-            // Left bottom triangle
-            -0.5f, 0.5f, 0f,
-            -0.5f, -0.5f, 0f,
-            0.5f, -0.5f, 0f,
-            // Right top triangle
-            0.5f, -0.5f, 0f,
-            0.5f, 0.5f, 0f,
-            -0.5f, 0.5f, 0f
-        };
-
-        // Sending data to OpenGL requires the usage of (flipped) byte buffers
-        FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
-        verticesBuffer.put(vertices);
-        verticesBuffer.flip();
-
-        vertexCount = 6;
-        
-        // Create a new Vertex Array Object in memory and select it (bind)
-        // A VAO can have up to 16 attributes (VBO's) assigned to it by default
-        vaoId = GL30.glGenVertexArrays();
-        GL30.glBindVertexArray(vaoId);
-
-        // Create a new Vertex Buffer Object in memory and select it (bind)
-        // A VBO is a collection of Vectors which in this case resemble the location of each vertex.
-        vboId = GL15.glGenBuffers();
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboId);
-        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, verticesBuffer, GL15.GL_STATIC_DRAW);
-        
-        // Put the VBO in the attributes list at index 0
-        GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
-        // Deselect (bind to 0) the VBO
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-
-        // Deselect (bind to 0) the VAO
-        GL30.glBindVertexArray(0);
-    }
- 
-    private void loop() {
-        // This line is critical for LWJGL's interoperation with GLFW's
-        // OpenGL context, or any context that is managed externally.
-        // LWJGL detects the context that is current in the current thread,
-        // creates the GLCapabilities instance and makes the OpenGL
-        // bindings available for use.
-        GL.createCapabilities();
- 
-        // Set the clear color
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
- 
-        // Run the rendering loop until the user has attempted to close
-        // the window or has pressed the ESCAPE key.
-        while ( glfwWindowShouldClose(window) == GLFW_FALSE ) {
-//            render();
+         
+        // init OpenGL here
+         
+        while (!Display.isCloseRequested()) {
+             
+            // render OpenGL here
+             
+            Display.update();
         }
-    }
- 
-    public void render() {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
- 
-        glfwSwapBuffers(window); // swap the color buffers
-
-        // Bind to the VAO that has all the information about the quad vertices
-        GL30.glBindVertexArray(vaoId);
-        GL20.glEnableVertexAttribArray(0);
-
-        // Draw the vertices
-        GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, vertexCount);
-
-        // Put everything back to default (deselect)
-        GL20.glDisableVertexAttribArray(0);
-        GL30.glBindVertexArray(0);
-        
-        // Poll for window events. The key callback above will only be
-        // invoked during this call.
-        glfwPollEvents();
+         
+        Display.destroy();
     }
     
     public static void main(String[] args) {
-        new TextureGeneration().run();
+        new TextureGeneration().start();
     }
  
 }
