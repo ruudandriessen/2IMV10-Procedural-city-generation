587b376b78e0a236a874be341c1d68dc66ba53c9
Ruud Andriessen <r.andriessen@ilysian.com>
2016-02-28 15:14:38 +0100

    Update to line editor

--- Unity/ProceduralCity/Assets/Source/Bezier/Line.cs
+++ Unity/ProceduralCity/Assets/Source/Bezier/Line.cs
@@ -3,14 +3,4 @@ using System.Collections;
 
 public class Line : MonoBehaviour {
 	public Vector3 p0, p1;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
