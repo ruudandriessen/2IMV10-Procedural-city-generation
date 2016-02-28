4b3833ca9d66a47a245a3417001dbe9b40f316f1
Ruud Andriessen <r.andriessen@ilysian.com>
2016-02-28 15:08:01 +0100

    Added line and line editor for future bezier curves

---
+++ Unity/ProceduralCity/Assets/Editor/LineEditor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Line))]
+public class LineInspector : Editor {
+	private void OnSceneGUI () {
+		Line line = target as Line;
+
+		Handles.color = Color.white;
+		Handles.DrawLine(line.p0, line.p1);
+	}
+}
---
+++ Unity/ProceduralCity/Assets/Source/Bezier/Line.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Line : MonoBehaviour {
+	public Vector3 p0, p1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
