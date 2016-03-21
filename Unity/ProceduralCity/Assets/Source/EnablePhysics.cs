965420dabc0dca438ab2a3ebae714d87f72d33ce
Ruud Andriessen <r.andriessen@ilysian.com>
2016-03-21 14:22:18 +0100

    EXPLOSIONS

--- Unity/ProceduralCity/Assets/Source/EnablePhysics.cs
+++ Unity/ProceduralCity/Assets/Source/EnablePhysics.cs
@@ -16,6 +16,13 @@ namespace ProceduralCity
 					Rigidbody body = go.GetComponent<Rigidbody>();
 					body.isKinematic = false;
 				}
+			} else if (Input.GetKeyDown("e")){
+				GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>() ;
+				foreach(GameObject go in allObjects) {
+					Rigidbody body = go.GetComponent<Rigidbody>();
+					body.isKinematic = false;
+					body.AddExplosionForce (100, Vector3.zero, 50);
+				}
 			}
 		}
 	}
