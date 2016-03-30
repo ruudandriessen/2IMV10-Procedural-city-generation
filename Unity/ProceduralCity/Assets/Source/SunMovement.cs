61763405408f52c427f62a016166817f640ec09e
Ruud Andriessen <r.andriessen@ilysian.com>
2016-03-30 22:10:32 +0200

    Added lightpost control

--- Unity/ProceduralCity/Assets/Source/SunMovement.cs
+++ Unity/ProceduralCity/Assets/Source/SunMovement.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class SunMovement : MonoBehaviour {
 
+	bool lighton = false;
+
 	// Use this for initialization
 	void Start () {
-	
+		Light[] allObjects = UnityEngine.Object.FindObjectsOfType<Light>() ;
+		foreach(Light light in allObjects) {
+			light.enabled = false;
+		}
+		GetComponent<Light> ().enabled = true;
 	}
 	
 	// Update is called once per frame
@@ -14,6 +20,13 @@ public class SunMovement : MonoBehaviour {
 			transform.Rotate (0.5f, 0.0f, 0.0f);
 		} else if (Input.GetKey (KeyCode.E)) {
 			transform.Rotate (-0.5f, 0.0f, 0.0f);
+		} else if (Input.GetKeyDown (KeyCode.R)) {
+			Light[] allObjects = UnityEngine.Object.FindObjectsOfType<Light>() ;
+			foreach(Light light in allObjects) {
+				light.enabled = !lighton;
+			}
+			GetComponent<Light> ().enabled = true;
+			lighton = !lighton;
 		}
 	}
 }
