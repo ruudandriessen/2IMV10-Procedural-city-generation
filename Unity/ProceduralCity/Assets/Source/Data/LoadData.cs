4da2391d821c1445f0d58a2ce5cc3852fe79b5a5
Rik Schreurs <r.schreurs@ilysian.com>
2016-04-18 22:54:36 +0200

    final commit

--- Unity/ProceduralCity/Assets/Source/Data/LoadData.cs
+++ Unity/ProceduralCity/Assets/Source/Data/LoadData.cs
@@ -11,7 +11,7 @@ public class LoadData {
 
 	// Use this for initialization
 	public static void Load () {
-		using(XmlTextReader reader = new XmlTextReader("Assets/Maps/herpt"))
+		using(XmlTextReader reader = new XmlTextReader("strijps"))
 //		using (XmlReader reader = XmlReader.Create("Assets/Maps/andorra"))
 		{
 			while (reader.Read())
